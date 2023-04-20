9b4b45ac0010df6f9dcf54eee4f6efc667184592
Ayoub Coding <120122307+AyoubCoding21@users.noreply.github.com>
2023-04-20 15:26:16 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <WeatherApp />
+  </div>
+</template>
+
+<script>
+import WeatherApp from './components/WeatherApp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WeatherApp
+  }
+}
+</script>
+
+<style>
+</style>
