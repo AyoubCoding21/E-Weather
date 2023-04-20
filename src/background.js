bcafd1a923aae56f0ff2de37dd24a1587f2ad35f
Ayoub Coding <120122307+AyoubCoding21@users.noreply.github.com>
2023-04-20 17:18:26 +0200

    Update background.js

--- src/background.js
+++ src/background.js
@@ -5,7 +5,6 @@ import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
-// Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
 ])
