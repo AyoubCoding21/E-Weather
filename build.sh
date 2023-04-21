09c05169cc3f43775cc1eae18ea130b5c4b0d840
Ayoub Coding <120122307+AyoubCoding21@users.noreply.github.com>
2023-04-21 18:28:41 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,5 @@
 echo -e "\e[0;33m${bold}Installing and building...\e[0m";
 sudo apt install npm -y --no-install-recommends;
-sleep 1s;
-if [ -d build/ ]
-then
-    sleep 1s;
-else
-    mkdir build;
-fi 
 npm install && npm audit fix --force
 npm run electron:build
-cp dist_electron/e-weather-1.0.0.AppImage build/
-rm dist_electron -rf
-./build/e-weather-1.0.0.AppImage
+./dist_electron/e-weather-1.0.0.AppImage
